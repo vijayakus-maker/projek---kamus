10e608f93cab5f7690c0ff903f6de984343c4bf6
vijayakus-maker <vijayakusuma303@gmail.com>
2026-03-13 21:24:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+import discord
+from discord.ext import commands
+from passgen import gen_pass
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
 
+@bot.command()
+async def gacha(ctx, count_heh = 5):
+    pahlawan = ['sudirman', 'sukarno', 'moh hatta']
+    await ctx.send(random.choice(pahlawan))
+    
+@bot.command()
+async def genpass(ctx, length = 5):
+    await ctx.send(gen_pass(length))    
+bot.run("Token")
