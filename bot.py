f7ab8a1d13744d9bac799e10dfbe40c0a8b7a84e
vijayakus-maker <vijayakusuma303@gmail.com>
2026-03-20 21:49:29 +0800

    Update bot.py
    penambahan gambar di bot

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
+import os
 import discord
 import random
 from discord.ext import commands
 from passgen import gen_pass
 import asyncio
+import requests
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
@@ -44,4 +46,22 @@ async def guess(ctx):
         await ctx.send('You are right!')
     else:
         await ctx.send(f'Oops. It is actually {answer}.')
+@bot.command()
+async def mem(ctx):
+    image_name = random.choice(os.listdir('images'))
+    with open('images/' + image_name, 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
 bot.run("Token")
